90be1414084fd66c501f254a60533f33d88d09a4
MKFMIKU <mikumkf@Gmail.com>
2017-06-11 13:19:57 +0800

    Add convert function

--- src/background.js
+++ src/background.js
@@ -4,14 +4,10 @@ var ajaxConfig = {
     "url": "https://try.kotlinlang.org/kotlinServer?type=convertToKotlin",
     "method": "POST",
     "headers": {
-        "origin": "https://try.kotlinlang.org",
-        "accept-encoding": "gzip, deflate, br",
         "x-requested-with": "XMLHttpRequest",
         "accept-language": "en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4",
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36",
         "content-type": "application/x-www-form-urlencoded",
         "accept": "text/plain, */*; q=0.01",
-        "referer": "https://try.kotlinlang.org/",
         "authority": "try.kotlinlang.org",
         "cache-control": "no-cache"
     },
@@ -21,11 +17,20 @@ var ajaxConfig = {
 };
 
 
-function convertJava(info) {
+function convertJava(info,tab) {
     ajaxConfig.data.text = info.selectionText;
-    $.ajax(ajaxConfig).done(function (response) {
-        console.log(response);
-    });
+    console.log(info.selectionText);
+    $.ajax(ajaxConfig)
+        .done(function (response) {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id, {kotlion: response});
+            });
+        })
+        .fail(function (err) {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id, {error: err});
+            });
+        });
 }
 
 var createMenuProp = {
